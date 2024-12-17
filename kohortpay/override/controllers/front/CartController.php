3344344c6a0e949054a9208bcf56c4b31e5a4946
Aymeric Aitamer <118519715+kohort-aymeric@users.noreply.github.com>
2024-12-17 14:23:12 +0100

    Implement referral input field (#63)
    * Improve compatibility 1.6
    
    * Implement referral input field

--- kohortpay/override/controllers/front/CartController.php
+++ kohortpay/override/controllers/front/CartController.php
@@ -34,7 +34,13 @@ class CartController extends CartControllerCore
       return;
     }
 
-    if (Tools::getIsset('addDiscount')) {
+    if (Tools::getIsset('applyReferralCode')) {
+      $referralCode = Tools::getValue('referral_code');
+      $this->validateReferralCode($referralCode);
+      return;
+    }
+
+    if (Tools::getIsset('addDiscount') || Tools::getIsset('submitDiscount')) {
       $code = trim(Tools::getValue('discount_name'));
       if (substr($code, 0, 3) === 'KHT') {
         $this->validateReferralCode($code);
